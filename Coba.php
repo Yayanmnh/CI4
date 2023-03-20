9633c819242c85e1a1714139c40fcb61b813306e
Yayanmnh <90267127+Yayanmnh@users.noreply.github.com>
2023-03-20 15:01:24 +0700

    baru
    baru juga

---
+++ Coba.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+class Coba extends BaseController
+{
+    public function index()
+    {
+        //return view('welcome_message');
+        echo "selamat datang Nyoba";
+    }
+    public function test($nama)
+    {
+        //return view('welcome_message');
+        echo "fungsi test.selamat datang " . $nama;
+    }
+    public function load()
+    {
+        $db = db_connect();
+        $query = $db->query('select * from iris_arff');
+
+        $row = $query->getRowArray();
+        $dbd = '';
+        if (isset($row)) {
+            $dbd = $dbd . $row['id'];
+            $dbd .= $row['petallength'];
+        }
+        $terserah = array(
+            'dbd' => $dbd,
+            'judul' => 'data iris',
+            'q' => $query,
+
+        );
+        return view('welcome_message', $terserah);
+    }
+}
